4583e3049dcd8623af81e9e91ce01763412ef814
Astha <99084211+Astha-Raghuwanshi@users.noreply.github.com>
2023-07-31 16:05:08 +0530

    week 1, q2
    Task: To create a simple program and upload it on github.

---
+++ lab1q2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "This is a test program to  be uploaded on github as a part of assignment assigned on 31st July 2023.";
+    cout << "As a part of OpenSource lab";
+    return 0;
+}
